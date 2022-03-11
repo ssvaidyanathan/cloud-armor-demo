305b725396bc9c32f9a126b8ba43e8f51f5522f4
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2022-03-11 13:26:56 -0800

    output

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
+output "network_name" {
+  value       = google_compute_network.vpc_network.name
+  description = "The name of the VPC being created"
+}
+
+output "network_self_link" {
+  value       = google_compute_network.vpc_network.self_link
+  description = "The URI of the VPC being created"
+}
+
+output "project_id" {
+  value       = google_compute_network.vpc_network.project
+  description = "VPC project id"
+}
+
+output "auto" {
+  value       = google_compute_network.vpc_network.auto_create_subnetworks
+  description = "The value of the auto mode setting"
+}
+
